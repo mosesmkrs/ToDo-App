44574c625bbc4c0865560f609d8f3e181cbe7de6
mosesmkrs <karisamoses392@gmail.com>
2024-01-20 12:13:35 +0300

    few corrections

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="flex w-full h-8 justify-center items-center p-10 border-t-2 border-b-2 border-white bg-gray-400 absolute bottom-0  ">
+      <h2 className="font-bold italic">
+        # Organize. Prioritize. Achieve. @MKRS
+      </h2>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,6 +4,7 @@ import { useDispatch } from "react-redux";
 import { addTodo, updateSearchTerm } from "../redux/actions";
 import FilterButtons from "./FilterButtons";
 import TodoList from "./TodoList";
+import Footer from "./Footer";
 
 function Todo() {
   const dispatch = useDispatch();
@@ -24,46 +25,49 @@ function Todo() {
     dispatch(updateSearchTerm(value));
   };
   return (
-    <div className="max-w-4xl mx-auto sm:mt-8 p-4 bg-gray-100 rounded">
-      <h2 className="mt-3 mb-6 text-2xl font-bold text-center uppercase">
-        PERSONAL TODO APP
-      </h2>
-      <div className="flex items-center mb-4">
-        <input
-          type="text"
-          name="addTodoInput"
-          id="addTodoInput"
-          value={newTodoText}
-          onChange={(e) => setNewTodoText(e.target.value)}
-          placeholder="Add Todo"
-          className="flex-grow p-2 border-b-2 border-gray-300 focus:outline-none focus:border-blue-500"
-        />
-        <button
-          onClick={handleAddTodoClick}
-          className="ml-4 p-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none"
-        >
-          <BsPlus />
-        </button>
-      </div>
-
-      <div className="flex items-center justify-between max-[580px]:block ">
-        <FilterButtons />
-        <div className="flex items-center mb-4 max-[580px]:justify-center">
+    <div className="min-h-full">
+      <div className="max-w-4xl mx-auto sm:mt-8 p-4 bg-gray-100 rounded">
+        <h2 className="Title mt-3 mb-6 text-2xl font-bold text-center uppercase ">
+          PERSONAL TODO APP
+        </h2>
+        <div className="flex items-center mb-4">
           <input
             type="text"
             name="addTodoInput"
             id="addTodoInput"
-            value={searchTerm}
-            onChange={(e) => handleSearchChange(e.target.value)}
-            placeholder="Search"
-            className="p-1.5 border-b-2 border-gray-300 focus:outline-none focus:border-blue-500 max-[580px]:mt-3 max-[400px]:pr-2  max-[580px]:pr-[30%]"
+            value={newTodoText}
+            onChange={(e) => setNewTodoText(e.target.value)}
+            placeholder="Add Todo"
+            className="flex-grow p-2 border-b-2 border-gray-300 focus:outline-none focus:border-blue-500"
           />
-          <button className="ml-4 p-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none">
-            <BsSearch />
+          <button
+            onClick={handleAddTodoClick}
+            className="ml-4 p-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none"
+          >
+            <BsPlus />
           </button>
         </div>
+
+        <div className="flex items-center justify-between max-[580px]:block ">
+          <FilterButtons />
+          <div className="flex items-center mb-4 max-[580px]:justify-center">
+            <input
+              type="text"
+              name="addTodoInput"
+              id="addTodoInput"
+              value={searchTerm}
+              onChange={(e) => handleSearchChange(e.target.value)}
+              placeholder="Search"
+              className="p-1.5 border-b-2 border-gray-300 focus:outline-none focus:border-blue-500 max-[580px]:mt-3 max-[400px]:pr-2  max-[580px]:pr-[30%]"
+            />
+            <button className="ml-4 p-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none">
+              <BsSearch />
+            </button>
+          </div>
+        </div>
+        <TodoList />
       </div>
-      <TodoList />
+      <Footer />
     </div>
   );
 }
